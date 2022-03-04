812ad38b91e904ffcf125631ceec1776a071e4a0
wdx <123@qq.com>
2022-03-04 15:17:36 +0800

    first commit

---
+++ src/main/java/com/getTest.java
@@ -0,0 +1,13 @@
+package com;
+
+/**
+ * @author wdx
+ * @create 2022-03-04 15:13
+ */
+public class getTest {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+
+
+    }
+}
