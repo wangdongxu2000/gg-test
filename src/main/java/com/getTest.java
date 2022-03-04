c7632e234396a1e60668b3051122300cd2991e80
wdx <123@qq.com>
2022-03-04 15:19:21 +0800

    third commit

--- src/main/java/com/getTest.java
+++ src/main/java/com/getTest.java
@@ -8,6 +8,11 @@ public class getTest {
     public static void main(String[] args) {
         System.out.println("hello git1");
         System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
+        System.out.println("hello git2");
+
+
 
 
     }
